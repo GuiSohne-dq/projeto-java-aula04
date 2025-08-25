4a7770d02bdbd8ad14c003d3fb83272cb761cb3d
Guilherme <guilherme.soares@duque.g12.br>
2025-08-25 19:41:35 -0300

    Primeiro commit da aplicação

---
+++ Aula04/src/main/java/com/guilherme/aula04/Aula04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.guilherme.aula04;
+
+/**
+ *
+ * @author 08304
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
